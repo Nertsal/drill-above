4671a624330915f7eb551387c06dae062ab50660
Nertsal <Nertsal@users.noreply.github.com>
2023-02-05 10:55:26 +0000

    deploy: f9322dce04a56092e8ca8f6d7c8f2c169b9ef703

--- dev/anlaut-3.js
+++ dev/anlaut-3.js
@@ -1420,31 +1420,31 @@ function getImports() {
         const ret = makeMutClosure(arg0, arg1, 256, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1240 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 522, __wbg_adapter_37);
-        return addHeapObject(ret);
-    };
     imports.wbg.__wbindgen_closure_wrapper1242 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 522, __wbg_adapter_37);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper1244 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 522, __wbg_adapter_42);
+        const ret = makeClosure(arg0, arg1, 522, __wbg_adapter_37);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper1246 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 522, __wbg_adapter_37);
+        const ret = makeMutClosure(arg0, arg1, 522, __wbg_adapter_42);
         return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_closure_wrapper1248 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 522, __wbg_adapter_37);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2340 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1250 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 522, __wbg_adapter_37);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_closure_wrapper2342 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 946, __wbg_adapter_49);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3430 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper3432 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 1031, __wbg_adapter_52);
         return addHeapObject(ret);
     };
